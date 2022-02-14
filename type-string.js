e89938c1c6917d07d8cd4379551df2869228fc1d
(VA) João Guilherme Lima <joaolima@cta.com.br>
2022-02-14 12:50:14 -0300

    String type

---
+++ type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo!';
+const senha = "SenhaSuperSegura456!";
+const stringNum = "15577"
+const citacao = 'O léo disse "Oi"';
+
+console.log(texto1)
+console.log(texto2)
+console.log(senha)
+console.log(stringNum)
+console.log(citacao)
+
+// Template string
+console.log(`asdasdasd ${citacao}`);
+
+// concatenação
+const meuNome = "Leonardo"
+console.log(citacao + " - " + meuNome )
