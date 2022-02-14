24e6b2a521ee54c380bde27debefa5214525ebb8
(VA) João Guilherme Lima <joaolima@cta.com.br>
2022-02-14 12:42:18 -0300

    Number type

---
+++ type-number.js
@@ -0,0 +1,21 @@
+// tipo Number
+
+const meuNumero = 3;
+const primeiroNum = 1;
+const segundoNum = 2;
+
+const operacaoMatematica = primeiroNum + segundoNum;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+const numPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOper = primeiroNum / numPontoFlutuante
+
+console.log(novaOper)
+
+// NaN -> not a number 
+const alura = "Alura";
+console.log(alura * primeiroNum);
